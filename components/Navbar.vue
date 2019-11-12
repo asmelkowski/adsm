996bbddf435fbede55f98ce239ded3cf086c7aef
Adam Smelkowski <adamsmelkowski@outlook.com>
2019-11-12 12:48:54 +0100

    Added navbar component

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,4 +1,5 @@
 <template>
+<div>
   <v-toolbar max-height="60">
     <v-toolbar-title>Title</v-toolbar-title>
 
@@ -12,14 +13,54 @@
       </v-toolbar-items>
     </template>
     <template v-else>
-      <v-icon><svg style="width:24px;height:24px" viewBox="0 0 24 24">
+      <svg @click.stop="drawer=!drawer" style="width:40px;height:40px;cursor:pointer" viewBox="0 0 24 24">
         <path fill="#ffffff" d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" />
-      </svg></v-icon>
+      </svg>
     </template>
   </v-toolbar>
+  <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+      right
+      mini-variant
+    >
+      <v-list-item>
+          <svg @click.stop="drawer=!drawer" style="width:40px;height:40px;cursor:pointer;" viewBox="0 0 24 24">
+    <path fill="#ffffff" d="M13.46,12L19,17.54V19H17.54L12,13.46L6.46,19H5V17.54L10.54,12L5,6.46V5H6.46L12,10.54L17.54,5H19V6.46L13.46,12Z" />
+</svg>
+      </v-list-item>
+
+      <v-list dense>
+
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title>Link 1</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title>Link 2</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title>Link 3</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+</div>
 </template>
 <script>
 export default {
-  name: 'Navbar'
+  name: 'Navbar',
+  data() {
+    return {
+        drawer: false
+    }
+  }
 }
 </script>
+<style scoped>
+</style>
